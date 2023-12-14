db0877ef1d9c4f89f7ac6684438aa573c4c2e316
Abdessattar <ghabi.1602@gmail.com>
2023-12-14 08:15:14 +0000

    Update the file

--- monty.h
+++ monty.h
@@ -22,4 +22,15 @@ typedef struct stack_s
 void fcheck(int, char **);
 void finterpret(char *);
 
+/**
+ * instruction_s - opcode and its function
+ * @opcode: operation code
+ * @f: function to handle the opcode
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
 #endif
